a372fcc1b339797091ceac74397bf34eb6d75787
lethanhbinhxq <hgutnj@gmail.com>
2026-01-29 09:30:24 +0700

    Update plugin config

--- back-end/exercise/ex-section-15/plugin/ckeditor/config.js
+++ back-end/exercise/ex-section-15/plugin/ckeditor/config.js
@@ -7,6 +7,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.filebrowserBrowseUrl = 'ckfinder/ckfinder.html';
-	config.filebrowserUploadUrl = 'ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files'
+	config.filebrowserBrowseUrl = 'plugin/ckfinder/ckfinder.html';
+	config.filebrowserUploadUrl = 'plugin/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files'
 };
