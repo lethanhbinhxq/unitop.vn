3d1fc0fe18f2ceb9bb1524358a336c9c2c65f439
lethanhbinhxq <hgutnj@gmail.com>
2026-02-02 10:30:03 +0700

    Fix ex 16

--- back-end/exercise/ex-section-16/pages/logout.php
+++ back-end/exercise/ex-section-16/pages/logout.php
@@ -3,5 +3,7 @@
 # Xử lý logout
 unset($_SESSION["is_login"]);
 unset($_SESSION["user_login"]);
+setcookie('is_login', false, time() - 3600);
+setcookie('user_login', false, time() - 3600);
 
 redirect_to("?page=login");
---
+++ back-end/exercise/ex-section-16/pages/product.php
@@ -0,0 +1,5 @@
+<?php get_header() ?>
+<div id="content">
+    <h1>Sản phẩm</h1>
+</div>
+<?php get_footer()?>
