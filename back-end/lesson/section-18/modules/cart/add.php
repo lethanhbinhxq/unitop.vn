7fe25626ce5fc636cd271af6e397a0551a32bbaa
lethanhbinhxq <binh.lethanh@hcmut.edu.vn>
2026-02-10 06:17:55 +0700

    Study shopping cart

--- back-end/lesson/section-18/modules/cart/add.php
+++ back-end/lesson/section-18/modules/cart/add.php
@@ -3,7 +3,8 @@
 # Lấy thông tin sản phẩm cần thêm vào giỏ hàng
 $id = (int)$_GET['id'];
 add_cart($id);
+// unset($_SESSION['cart']);
 
+// show_data($_SESSION['cart']);
 
-
-show_data($_SESSION['cart']);
+redirect("?mod=cart&act=show");
