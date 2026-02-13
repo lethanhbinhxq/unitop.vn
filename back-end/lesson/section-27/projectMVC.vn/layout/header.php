2194f9a56b26fb2811542c5eb0e1cb63090ccafc
lethanhbinhxq <hgutnj@gmail.com>
2026-02-13 09:04:11 +0700

    Init user manager mvc project

---
+++ back-end/lesson/section-27/projectMVC.vn/layout/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hệ thống MVC</title>
+    <link rel="stylesheet" href="public/css/reset.css">
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <ul id="main-menu">
+                <li><a href="?">Trang chủ</a></li>
+                <li><a href="?mod=page&controller=index&action=detail&id=1">Giới thiệu</a></li>
+                <li><a href="?mod=users&controller=index">Thành viên</a></li>
+            </ul>
+        </div>
+        <!-- End header -->
---
+++ back-end/lesson/section-27/projectMVC.vn/modules/page/controllers/indexController.php
@@ -0,0 +1,8 @@
+<?php
+
+function construct() {
+}
+
+function detailAction() {
+    load_view('detail');
+}
