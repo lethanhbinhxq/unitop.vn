cea47add0fd070679b7deb1af1a89f079f860cdd
lethanhbinhxq <hgutnj@gmail.com>
2026-04-24 10:14:27 +0700

    Fix exercise to do list

--- jsBeginner.com/exercises/todolist/app.js
+++ jsBeginner.com/exercises/todolist/app.js
@@ -2,6 +2,7 @@ const LIST_TASK_DATA = "LIST_TASK_DATA";
 const task_list = document.getElementById("task_list");
 const input_task = document.getElementById("input_task");
 const submit_task = document.getElementById("submit_task");
+const input_form = document.forms.input_form;
 
 function getData() {
     let data = localStorage.getItem(LIST_TASK_DATA);
@@ -22,24 +23,33 @@ function getNumCompleted(data) {
     return count;
 }
 
+function createTaskItem(name, is_complete, index) {
+    return `<li class="task_item" index="${index}">
+                <span class="task_name ${is_complete ? 'completed' : ''}" onClick="toggleCompleted(${index})">${name}</span>
+                <div class="action">
+                    <button onClick="pushEditTask(this)" class="action_edit">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                        </svg>
+                    </button>
+                    <button onClick="deleteTask(this)" class="action_delete">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+            </li>`;
+}
+
 function render() {
     let data = getData();
-    let html = "";
-    for (let index in data) {
-        html += `
-                <li class="task_item" index="${index}">
-                    <span class="task_name ${data[index].isCompleted ? 'completed' : ''}" onClick="toggleCompleted(${index})">${data[index].name}</span>
-                    <div class="action">
-                        <button onClick="editTask(this)" class="action_edit"><i class="fa-solid fa-pencil"></i></button>
-                        <button onClick="deleteTask(this)" class="action_delete"><i class="fa-solid fa-trash-can"></i></button>
-                    </div>
-                </li>`;
-    }
-    task_list.innerHTML = html;
+    let html = data.map((element, index) => {
+        return createTaskItem(element.name, element.isCompleted, index);
+    })
+    task_list.innerHTML = html.join('');
 
     const num_completed = getNumCompleted(data);
     const numCompletedEl = document.getElementById('num_completed');
-    console.log(num_completed);
 
     if (num_completed > 0) {
         numCompletedEl.innerText = `Yeah, ${num_completed} task(s) completed!`;
@@ -48,26 +58,25 @@ function render() {
     }
 }
 
-function addTask() {
-    const input_task = document.getElementById("input_task").value;
+function addTask(new_task) {
     const data = getData();
-    data.push({
-        name: input_task,
-        isCompleted: false,
-    })
+    data.push(new_task)
     saveData(data);
 }
 
-function editTask(element) {
+function pushEditTask(element) {
     const index = element.closest('.task_item').getAttribute('index');
     const data = getData();
     input_task.value = data[index].name;
     submit_task.value = 'EDIT TASK';
-    const task_index = document.createElement('div');
-    task_index.id = 'task_index';
-    task_index.setAttribute('index', index);
-    task_list.appendChild(task_index);
-    element.closest('.action').style.visibility = 'hidden';
+    input_task.setAttribute('index', index);
+}
+
+function editTask(name, index) {
+    const data = getData();
+    data[index].name = name;
+    saveData(data);
+    input_task.removeAttribute('index');
 }
 
 function deleteTask(element) {
@@ -85,22 +94,35 @@ function toggleCompleted(index) {
     saveData(data);
 }
 
-const input_form = document.forms.input_form;
 input_form.addEventListener('submit', (e) => {
     // localStorage.removeItem(LIST_TASK_DATA);
-    const task_index = document.getElementById('task_index');
-    if (task_index) {
-        const index = task_index.getAttribute('index');
-        const data = getData();
-        data[index].name = input_task.value;
-        saveData(data);
-        task_list.removeChild(task_index);
+    const index = input_task.getAttribute('index');
+
+    if (input_task.value.length < 2) {
+        alert('Enter your task!');
+        return false;
+    }
+    if (index) {
+        editTask(input_task.value, index);
     } else {
-        addTask();
+        const input_task = document.getElementById("input_task").value;
+        const new_task = {
+            name: input_task,
+            isCompleted: false,
+        }
+        addTask(new_task);
     }
     input_task.value = "";
     submit_task.value = "ADD TASK";
     e.preventDefault();
+})
+
+document.addEventListener('keyup', (e) => {
+    if (e.which == 27) {
+        input_task.value = "";
+        submit_task.value = "ADD TASK";
+        input_task.removeAttribute('index');
+    }
 })
 
 render();
